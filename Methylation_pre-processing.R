8d6f53951453e91dfd2cba3c5c60e289d9bd3205
V-Catherine <voegelec@iarc.fr>
2020-06-10 16:41:33 +0200

    Update Methylation_pre-processing.R

--- Methylation_pre-processing.R
+++ Methylation_pre-processing.R
@@ -316,24 +316,3 @@ write.table(targets, file="TargetsFile.csv", sep=",", col.names=NA)
 write.table(betaNorm, file="NormalisedFilteredBetaTable.csv", sep=",", col.names=NA)
 write.table(mNorm, file="NormalisedFilteredMTable.csv", sep=",", col.names=NA)
 write.table(mNoInf, file="NormalisedFilteredMTable_noInf.csv", sep=",", col.names=NA)
-
-
-
-
-################
-# Principal component regression analysis plots
-###############
-# select variables of interest
-head(pData(fun_filtered))
-pData2 <- data.frame(pData(fun_filtered))
-pData2$sentrix_id = as.factor(pData2$sentrix_id)
-
-lev = apply(pData2,2,function(x) table(x))
-pData2 = pData2[,(sapply(lev,max,na.rm=T)>1)&(sapply(lev,nrow)>1) ]
-
-#svg("PCA.svg",h=6,w=6)
-pcrplot(minfi::getBeta(fun_filtered), cov= pData2, npc=20)
-#dev.off()
-
-# write session info
-writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
