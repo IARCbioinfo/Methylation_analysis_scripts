38aa2e70d53a35862e512f8af7bcf18deabab00d
ASextonOates <53007238+ASextonOates@users.noreply.github.com>
2020-06-11 12:54:00 +0200

    Add removal of low intensity samples

--- Methylation_pre-processing.R
+++ Methylation_pre-processing.R
@@ -91,6 +91,16 @@ par(mfrow=c(1,1),family="Times",las=1)
 plotQC(qc) # If U and/or M intensity log medians are <10.5, sample is by default of bad quality
 dev.off()
 
+if(sum(rowMeans(as.data.frame(qc))<10.5)>0) print(paste("Warning: sample", rownames(qc)[rowMeans(as.data.frame(qc))<10.5], "has mean meth-unmeth signal intensity <10.5"))
+
+# If required: remove any samples with meth or unmeth intensity < 10.5, then remove from all previous files
+keep.samples <- apply(as.data.frame(qc),1,mean) > 10.5
+RGSet <- RGSet[,keep.samples]
+MSet <- MSet[,keep.samples]
+ratioSet <- ratioSet[,keep.samples]
+gset <- gset[,keep.samples]
+targets <- targets[keep.samples,]
+
 # D) Calculate detection p values and plot 
 # Here the samples are coloured by sentrix ID to check for poor performing chips
 detP <- detectionP(RGSet)
@@ -102,7 +112,7 @@ dev.off()
 
 if(sum(colMeans(detP)>0.01)>0 ) print(paste("Warning: sample", names(colMeans(detP))[colMeans(detP)>0.01], "has >0.01 mean detection p-value" ) )
 
-# Remove any samples with detection p value > 0.01, then remove from all previous files
+# If required: remove any samples with detection p value > 0.01, then remove from all previous files
 keep.samples <- apply(detP,2,mean) < 0.01 
 RGSet <- RGSet[,keep.samples]
 MSet <- MSet[,keep.samples]
